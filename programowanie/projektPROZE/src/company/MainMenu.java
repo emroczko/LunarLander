b64b3936d430d3888e18224bcef7f5206ffd4005
Eryk Mroczko <e.mroczko@stud.elka.pw.edu.pl>
2020-04-05 19:23:51 +0200

    5.04

--- programowanie/projektPROZE/src/company/MainMenu.java
+++ programowanie/projektPROZE/src/company/MainMenu.java
@@ -1,5 +1,7 @@
 package company;
 
+import javafx.scene.layout.BackgroundImage;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -25,7 +27,8 @@ public class MainMenu extends JFrame {
 
     public MainMenu()
     {
-        Frame test =new Frame();
+
+        Frame test =new JFrame();
         String bg_path = "Images/MainMenu.png";
         load_image(bg_path);
 
@@ -34,7 +37,9 @@ public class MainMenu extends JFrame {
         Container components_container = new JLabel(new ImageIcon(bg_path));
 
         components_container.setLayout(new GridBagLayout());
+
         GridBagConstraints gbc = new GridBagConstraints();
+
 
         Button b_play = new Button("Play");
         b_play.addActionListener(new ActionListener() {
@@ -42,7 +47,14 @@ public class MainMenu extends JFrame {
                                     nickScreen();
                                      }
                                  });
+
         Button b_best_scores = new Button("Best Scores");
+        b_best_scores.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                bestScoreScreen();
+            }
+        });
+
 
 
         Button b_instructions = new Button("Instructions");
@@ -52,9 +64,13 @@ public class MainMenu extends JFrame {
 
         b_play.setFont(font);
         b_play.setBackground(Color.black);
+        //b_play.setMnemonic(KeyEvent.VK_D);
+        b_play.setActionCommand("disable");
 
         b_best_scores.setFont(font);
         b_best_scores.setBackground(Color.black);
+       // b_best_scores.setMnemonic(KeyEvent.VK_M);
+        b_best_scores.setVisible(true);
 
         b_instructions.setFont(font);
         b_instructions.setBackground(Color.black);
@@ -69,6 +85,7 @@ public class MainMenu extends JFrame {
         gbc.gridx = 0;
         gbc.gridy = 2;
         gbc.anchor = GridBagConstraints.SOUTH;
+        gbc.insets = new Insets(15, 15, 15, 15);
         components_container.add(b_best_scores, gbc);
 
         gbc.gridx = 0;
@@ -79,22 +96,45 @@ public class MainMenu extends JFrame {
         this.add(components_container);
         this.setVisible(true);
 
+        this.repaint();
         this.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent evt) {System.exit(0);
             }
         });
     }
+    void bestScoreScreen(){
+        this.getContentPane().removeAll();
+        load_image("Images/SaturnMoon.png");
+        Container components_container = new JLabel(new ImageIcon("Images/SaturnMoon.png"));
+
+
+        //Button button = new Button("chuj");
+        this.getContentPane().setLayout(new FlowLayout());
+
+        JButton button1 = new JButton();
+        button1.setText("Java Code Geeks");
+
+        JButton button2 = new JButton("Java Examples & Code Snippets");
+
+        // add buttons to frame
+        add(button1);
+        add(button2);
+        this.repaint();
+        this.setVisible(true);
+
+    }
     void nickScreen(){
         this.getContentPane().removeAll();
         this.repaint();
         this.setLayout(new GridBagLayout());
+
         String bg_path = "Images/MainMenu.png";
         Container components_container = new JLabel(new ImageIcon(bg_path));
 
         components_container.setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
 
-        Label typeNick=new Label("Type your nick");
+            Label typeNick=new Label("Type your nick");
         TextField enterName = new TextField("Your nick...");
         Button b_start = new Button("Start!");
         b_start.addActionListener(new ActionListener() {
@@ -157,8 +197,77 @@ public class MainMenu extends JFrame {
     }
     void loadLevel(int levelNumber){
         this.getContentPane().removeAll();
+        this.repaint();
         timer = new Timer();
+        this.setLayout(new GridBagLayout());
+
+        String imgPath = whichLevel(levelNumber);
+        load_image(imgPath);
+        Container components_container = new JLabel(new ImageIcon(imgPath));
+
+        components_container.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        Button exitButton = new Button("Wyjście");
+       // Button pauseButton =  new Button("Pauza");
+        Label vy = new Label("999999");
+        Label vx = new Label("999999");
+        Label fuelLabel = new Label("Paliwo: 99999");
+        Label spaceships = new Label("Pozostałe statki: 99");
+        Label typeNick=new Label("chuj");
+        TextField enterName = new TextField("nie dziala");
+        JButton pauseButton = new JButton("||");
+        Button b_back = new Button("Back");
+
+        Font font = new Font("uni 05_53", Font.PLAIN, 20);
+
+        typeNick.setBackground(Color.black);
+        typeNick.setFont(font);
+        typeNick.setForeground(Color.lightGray);
+
+        enterName.setBackground(Color.black);
+        enterName.setFont(font);
+        enterName.setForeground(Color.YELLOW);
+        //enterName.setHorizontalAlignment(JTextField.CENTER);
+
+        pauseButton.setFont(font);
+        pauseButton.setForeground(Color.white);
+       // pauseButton.setBackground(Color.black);
+       pauseButton.setOpaque(false);
+       pauseButton.setContentAreaFilled(false);
+        pauseButton.setBorderPainted(false);
+
+        b_back.setFont(font);
+        b_back.setBackground(Color.black);
+
+
+
+        gbc.gridwidth = 3;
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.insets = new Insets(15, 15, 15, 15);
+        components_container.add(typeNick, gbc);
+
+        gbc.gridwidth = 2;
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+
+        components_container.add(enterName, gbc);
+
+        gbc.gridx = -7;
+        gbc.gridy = -8;
+        components_container.add(pauseButton, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        components_container.add(b_back, gbc);
+        this.add(components_container);
+        this.setVisible(true);
+    }
+
+
+    String whichLevel(int levelNumber){
         String imgPath;
+
         switch (levelNumber) {
             case 1:
                 imgPath = "Images/SaturnMoon.png";
@@ -167,7 +276,7 @@ public class MainMenu extends JFrame {
                 imgPath = "Images/MainMenu.png";
                 break;
             case 3:
-                imgPath = "Images/SaturnMoon.png";
+                imgPath = "Images/EarthMoon.png";
                 break;
             case 4:
                 imgPath = "Images/SaturnMoon.png";
@@ -187,24 +296,51 @@ public class MainMenu extends JFrame {
             default:
                 imgPath = "Images/SaturnMoon.png";
         }
+        return imgPath;
+    }
+        /*this.getContentPane().removeAll();
+
+        this.setLayout(new GridBagLayout());
+
+
 
         load_image(imgPath);
         this.repaint();
 
-        Container components_container1 = new JLabel(new ImageIcon(imgPath));
-        components_container1.setLayout(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
 
+        Container sideMenu = new JLabel(new ImageIcon(imgPath));
+        sideMenu.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        //Label vspeed = new Label("V. speed: ");
+
+
+        sideMenu.add(vx);
+        sideMenu.add(vy);
+        sideMenu.add(exitButton);
+
+        sideMenu.add(spaceships);
+        sideMenu.add(fuelLabel);
+        //components_container1.add(vspeed);
+        Font font = new Font("uni 05_53", Font.PLAIN, 20);
+
+        pauseButton.setBackground(Color.black);
+        pauseButton.setFont(font);
+        pauseButton.setForeground(Color.lightGray);
+        gbc.gridwidth = 3;
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.insets = new Insets(15, 15, 15, 15);
+        sideMenu.add(pauseButton,gbc);
         Level level = new Level(levelNumber);
         timer.scheduleAtFixedRate(level.new Animation_Timer(), 100, 30);
-
-    }
+        this.setVisible(true);
+    }*/
     public void load_image(String path)
     {
         try
         {
             img =  ImageIO.read(new File(path));
-            System.out.println(img);
+            //System.out.println(img);
         }
         catch(Exception e){e.printStackTrace();}
     }
@@ -212,8 +348,8 @@ public class MainMenu extends JFrame {
     @Override
     public void paint(Graphics g){
         super.paint(g);
-        g.drawImage(img, 0, 0, getWidth(), getHeight(), this);
-
+        g.drawImage(img, 0, 0,getWidth(), getHeight(),this);
+        //super.paint(g);
     }
 
 
---
+++ programowanie/projektPROZE/src/company/backgroundImage.java
@@ -0,0 +1,24 @@
+package company;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+
+public class backgroundImage extends JPanel{
+    Image image;
+    public backgroundImage(String path) {
+        try {
+            image = ImageIO.read(new File(path));
+            //System.out.println(img);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public void paint(Graphics g){
+        super.paint(g);
+        g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+    }
+
+}
