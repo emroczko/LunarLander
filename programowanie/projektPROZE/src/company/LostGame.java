fe59e52918dd105d9589b86db56b42d530c5a26c
Eryk Mroczko <e.mroczko@stud.elka.pw.edu.pl>
2020-04-23 21:12:14 +0200

    23.04.20

--- programowanie/projektPROZE/src/company/LostGame.java
+++ programowanie/projektPROZE/src/company/LostGame.java
@@ -16,25 +16,39 @@ public class LostGame extends JPanel {
     private String nick;
     /** Zmienne przechowująca wielkość poprzedniego okna*/
     private int a, b;
+    /**Ilość punktów zdobytych przez gracza**/
     private int points;
-
+    /** Kolor (niebieski) czcionki używanej w oknie*/
     Color aqua = new Color (51, 134, 175);
+    /** Kolor (żółty) czcionki używanej w oknie*/
     Color citron = new Color (223, 234, 24);
-
+    /** Przycisk zagrania jeszcze raz*/
     JButton startButton = new JButton("Play again");
+    /** Przycisk powrotu do głównego menu*/
     JButton backButton = new JButton("Return to main menu");
+    /** Napis informujący o niepowodzeniu*/
     JLabel lost = new JLabel("THE MISSION HAS FAILED");
+    /** Napis informujący o ilości zdobytych punktów*/
     JLabel pointsLabel = new JLabel();
-
+    /** Obiekt klasy LabelCustomizer*/
     LabelCustomizer customLabelAqua = new LabelCustomizer(aqua, 40);
-    LabelCustomizer customLabelWhite = new LabelCustomizer(aqua, 40);
+    /** Obiekt klasy LabelCustomizer*/
+    LabelCustomizer customLabelWhite = new LabelCustomizer(Color.WHITE, 40);
+    /** Obiekt klasy ButtonCustomizer*/
     ButtonCustomizer customButton = new ButtonCustomizer(true, citron, 32);
-
     /** Obiekt klasy GridBagConstraintsMaker**/
     GridBagConstraintsMaker customGBC = new GridBagConstraintsMaker();
     /** Obiekt klasy NewWindow **/
     NewWindow newWindow = new NewWindow();
 
+    /**
+     * Konstruktor klasy dodający przyciski, ustawiający poczatkowy rozmiar okna
+     * oraz przypisujący pobrane wartości do odpowiednich pól w klasie
+     * @param xSize - szerokość poprzedniego okna
+     * @param ySize - wysokość poprzedniego okna
+     * @param earnedPoints - ilość zdobytych punktów
+     * @param nickName - nick gracza
+     */
     public LostGame(int xSize, int ySize, int earnedPoints, String nickName){
         this.removeAll();
         repaint();
@@ -54,22 +68,21 @@ public class LostGame extends JPanel {
         backButton.addActionListener(returnToMainMenuButtonListener());
 
         pointsLabel.setText("Your score is: "+ earnedPoints);
+
         customButton.customizer(startButton);
         customButton.customizer(backButton);
         customLabelAqua.customizer(lost);
         customLabelWhite.customizer(pointsLabel);
-
 
         this.add(lost, customGBC.gbcCustomize(0,2, 0, 0,3, "none"));
         this.add(pointsLabel, customGBC.gbcCustomize(0,3, 0, 0,3, "none"));
         this.add(startButton, customGBC.gbcCustomize(0,4, 0, 0,3, "none"));
         this.add(backButton, customGBC.gbcCustomize(0,5, 0, 0,3, "none"));
         customGBC.gbcCustomize(0,0,1,1,0,"none");
-
-
-
         }
-
+    /**
+     * Metoda próbująca zapisać nick oraz punkty gracza do pliku z pomoca metody klasy RankingServer
+     */
     private void save(){
         try {
             RankingSaver.saveToFile(nick, points);
@@ -100,7 +113,6 @@ public class LostGame extends JPanel {
                 public void actionPerformed(ActionEvent e) {
                     cleanWindow();
                     add(new IntroLevel(getWidth(),getHeight(),1, PropertiesLoad.numberOfLives, 0, nick),newWindow.buttonsClickedBehaviour());
-
                 }
             };
             return actionListener;
@@ -118,7 +130,9 @@ public class LostGame extends JPanel {
             };
             return actionListener;
         }
-
+    /**
+     * Odpowiada za wywołanie metody obiektu klasy NewWindow służącej do usunięcia wszystkich elemntów z obecnego JPanelu
+     */
     private void cleanWindow(){
         newWindow.layoutMakerLostGame(this);
     }
