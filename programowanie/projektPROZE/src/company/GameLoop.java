556b9e0be4799ce206e0b3f1c736fc688eeb89d6
Eryk Mroczko <e.mroczko@stud.elka.pw.edu.pl>
2020-04-23 20:11:37 +0200

    23.04.16

--- programowanie/projektPROZE/src/company/GameLoop.java
+++ programowanie/projektPROZE/src/company/GameLoop.java
@@ -5,11 +5,15 @@ import java.awt.event.ActionListener;
 /**
  * Klasa odpowiedzialna za obsługę zdarzeń gry
  */
-
 public class GameLoop implements ActionListener{
 
     /** obiekt klasy Level*/
     private Level level;
+
+    /**
+     * Konstruktor przypisujący pobrany obiekt do obiektu stworzonego w tej klasie
+     * @param level - obiekt klasy Level (bieżący poziom)
+     */
     public GameLoop(Level level) {
         this.level = level;
     }
