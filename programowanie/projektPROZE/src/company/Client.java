76437ca5ec9f6500b1e563e57ea3db393cbaa9fa
Jan Konrad Szewczyński <j.szewczynski@stud.elka.pw.edu.pl>
2020-05-06 20:42:09 +0200

    server

--- programowanie/projektPROZE/src/company/Client.java
+++ programowanie/projektPROZE/src/company/Client.java
@@ -4,11 +4,26 @@ import java.io.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+/**
+ * Klasa odpowiedzialna za kontakt z serwerem. Wysyła zapytania o udostępniennie danych konfiguracyjnych i rankingu
+ */
 public class Client {
+
     private static Socket socket;
+    /**Zmienna przechowująca adres ip serwera*/
     static String Address;
+    /**Zmienna przechowująca numer portu serwera*/
     static int Port;
-    static boolean online;
+    /**Zmienna określająca czy jesteśmy online*/
+    static boolean online = false;
+
+    /**
+     * Tworzy łącze z serwerem, tworzy obiekt klasy socket.
+     * @param address adres ip serwera
+     * @param port numer portu serwera
+     * @throws UnknownHostException
+     * @throws IOException
+     */
     static void Connect(String address, int port) throws UnknownHostException, IOException {
         socket = new Socket(address, port);
         Address = address;
@@ -16,6 +31,13 @@ public class Client {
         System.out.println("Connected");
 
     }
+
+    /**
+     * Wysyła zapytania do serwera
+     * @param command komenda odpowiadająca zapytaniu które chcemy wysłać do serwera
+     * @return zwraca odpowiedź od serwera
+     * @throws IOException
+     */
     static String getProperty(String command) throws IOException {
         try {
             socket = new Socket(Address, Port);
@@ -27,11 +49,24 @@ public class Client {
         BufferedReader buf = new BufferedReader(new InputStreamReader(in));
         return buf.readLine();
     }
+
+    /**
+     * Wysyła do serwera zapytanie o dane konfiguracyjne przy uzyciu metody getProperty z odpowiednią komendą
+     * @return zwraca dane konfiguracyjne pobrane z serwera
+     * @throws IOException
+     */
     static String getConfigs() throws IOException{
         String configs = getProperty("GetConfigs");
         socket.close();
         return configs;
     }
+
+    /**
+     * Wysyła do serwera zapytanie o dane konfiguracyjne przy uzyciu metody getProperty z odpowiednią komendą
+     * @param levelNumber numer poziomu którego dane konfiguracyjne chcemy otrzymać
+     * @return zwraca dane konfiguracyjne poziomu
+     * @throws IOException
+     */
     static String getLevel(int levelNumber) throws IOException{
         String levelConfigs = getProperty("GetLevel-" + levelNumber);
         socket.close();
