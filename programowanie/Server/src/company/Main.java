ae53bd8b74bd362e3956e0d4b5a440f29bb19f4b
Eryk Mroczko <e.mroczko@stud.elka.pw.edu.pl>
2020-05-08 14:51:42 +0200

    8.05.1

--- programowanie/Server/src/company/Main.java
+++ programowanie/Server/src/company/Main.java
@@ -8,9 +8,8 @@ public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         EventQueue.invokeLater(() -> {
             try {
-                //Server server = new Server();
-                //server.run();
                 new MainFrame();
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
